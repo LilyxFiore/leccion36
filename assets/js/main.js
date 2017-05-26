8358888fbea361358918f916dd5cb491279ce4fe
LilyxFiore <liliana.fiorela@gmail.com>
2017-05-25 21:28:49 -0500

    subiendo js modificado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,44 @@
 function initMap() {
     var laboratoriaLima = {
-        lat: -12.1191427, lng: -77.0349046};
+        lat: -12.1191427,
+        lng: -77.0349046
+    };
 
-    var map = new google.maps.Map(document.getElementById("map"),
-        { zoom: 18, center: laboratoriaLima});
+    var map = new google.maps.Map(document.getElementById("map"),{
+      zoom: 18,
+      center: laboratoriaLima
+    });
 
     var markadorLaboratoria = new google.maps.Marker({
         position: laboratoriaLima,
         map: map
     });
-}
 
-function buscar() {
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-    }
-}
-
-var funcionExito = function (posicion) {
     var latitud, longitud;
-    latitud = posicion.coords.latitude;
-    longitud = posicion.coords.longitude;
 
+    var funcionExito = function (posicion) {
 
-    var map = new google.maps.Map(document.getElementById("map"));
-    map.setZoom(18);
-    map.setCenter({lat: latitud, lng: longitud});
+        latitud = posicion.coords.latitude;
+        longitud = posicion.coords.longitude;
 
-    var miUbicacion = new google.maps.Marker({
-        position: {lat: latitud, lng: longitud},
-        map: map
-    });
+        var miUbicacion = new google.maps.Marker({
+            position: {lat: latitud, lng: longitud},
+            map: map
+        });
+
+        map.setZoom(18);
+        map.setCenter({lat: latitud, lng: longitud});
+    }
+
+    var funcionError = function (error) {
+        alert("Tenemos un poblema con encontrar tu ubicación");
+    }
+
+    function buscar() {
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+        }
+    }
+      
+    document.getElementById("encuentrame").addEventListener("click",buscar);
 }
-
-
-
-var funcionError = function (error) {
-    alert("Tenemos un poblema con encontrar tu ubicación");
-}
-
-document.getElementById("encuentrame").addEventListener("click",buscar);
